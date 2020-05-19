cf100944e2a721468d63b6eda2eb23298d8b72a5
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-19 12:29:11 +0300

    deploy

--- src/templates/main.template.js
+++
@@ -1,6 +0,0 @@
-import { html, render } from 'lit-html';
-const mainSection = document.getElementById('main');
-const name = (name) => html` <h1>${name}</h1> `;
-render(name('Rowad'), mainSection);
-
-export default { displayName: name };
