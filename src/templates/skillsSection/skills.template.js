a64d63e70dba219106b5dd82e9592797bef4d2e8
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-19 19:51:40 +0300

    more skill icons with links

--- src/templates/skillsSection/skills.template.js
+++ src/templates/skillsSection/skills.template.js
@@ -2,37 +2,47 @@ import { html, render } from 'lit-html';
 import '@fortawesome/fontawesome-free/js/all';
 const linksSection = document.getElementById('skills');
 const icons = [
-  'js',
-  'node',
-  'npm',
-  'python',
-  'php',
-  'angular',
-  'vuejs',
-  'react',
-  'aws',
-  'docker',
-  'git-alt',
-  'gitkraken',
-  'bitbucket',
-  'ember',
-  'github-alt',
-  // 'gulp',
-  // 'grunt',
-  'html5',
-  'jenkins',
-  'jira',
-  'less',
-  'sass',
-  'linux',
-  'ubuntu',
-  'windows',
-  'slack',
-  'skype',
+  {
+    icon: 'js',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+  },
+  { icon: 'node', link: 'https://nodejs.org/en/' },
+  { icon: 'npm', link: 'https://www.npmjs.com/' },
+  { icon: 'python', link: 'https://www.python.org/' },
+  { icon: 'php', link: 'https://www.php.net/' },
+  { icon: 'laravel', link: 'https://laravel.com/' },
+  { icon: 'angular', link: 'https://angular.io/' },
+  { icon: 'vuejs', link: 'https://vuejs.org/' },
+  { icon: 'react', link: 'https://reactjs.org/' },
+  { icon: 'aws', link: 'https://aws.amazon.com/' },
+  { icon: 'docker', link: 'https://www.docker.com/' },
+  { icon: 'git-alt', link: 'https://git-scm.com/' },
+  { icon: 'gitkraken', link: 'https://www.gitkraken.com/' },
+  { icon: 'bitbucket', link: 'https://bitbucket.org/product' },
+  // {icon: 'ember', link: ''},
+  { icon: 'github-alt', link: 'http://github.com/' },
+  // { 'gulp', link: ''},
+  //{ 'grunt', link: ''},
+  {
+    icon: 'html5',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5',
+  },
+  { icon: 'css3', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
+  { icon: 'jenkins', link: 'https://www.jenkins.io/' },
+  { icon: 'jira', link: 'https://www.atlassian.com/software/jira' },
+  { icon: 'less', link: 'http://lesscss.org/' },
+  { icon: 'sass', link: 'https://sass-lang.com/documentation/syntax' },
+  { icon: 'linux', link: 'https://www.linux.org/' },
+  { icon: 'ubuntu', link: 'https://ubuntu.com/' },
+  { icon: 'windows', link: 'https://www.microsoft.com/en-us/windows' },
+  { icon: 'slack', link: 'https://slack.com/' },
+  { icon: 'skype', link: 'https://www.skype.com/en/' },
+  { icon: 'facebook-square', link: 'https://developers.facebook.com/' },
+  { icon: 'google-plus-g', link: 'https://developers.google.com/' },
 ];
 const skills = icons.map(
-  (icon) => html`<div class="one columns center">
-    <a href="#"><i class="fab fa-${icon} fa-5x"></i></a>
+  ({ icon, link }) => html`<div class="one columns center">
+    <a href="${link}" target="_blank"><i class="fab fa-${icon} fa-5x"></i></a>
   </div>`
 );
 
