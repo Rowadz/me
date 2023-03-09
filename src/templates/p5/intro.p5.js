170f35c1c98b8bf869902afbd1f54f6993122cf9
MohammedAl-rowad <rowadwo@gmail.com>
2023-03-09 23:26:56 +0100

    handle resize in a good way"

--- src/templates/p5/intro.p5.js
+++ src/templates/p5/intro.p5.js
@@ -1,43 +1,43 @@
-import * as p5 from 'p5';
-import { Vector } from 'p5';
-import { Particle } from './Particle.p5';
-import { sexyBlack } from '../colorsHelper/colors.helper';
-window.p5 = p5;
+import * as p5 from 'p5'
+import { Vector } from 'p5'
+import { Particle } from './Particle.p5'
+import { sexyBlack } from '../colorsHelper/colors.helper'
+window.p5 = p5
 let s = (sk) => {
-  const scl = 10;
+  const scl = 10
   let cols,
     rows,
     zoff = 0,
     flowfield = [],
     inc = 0.1,
-    particles = [];
+    particles = []
   sk.setup = () => {
-    const { innerHeight, innerWidth } = window;
-    const canves = sk.createCanvas(innerWidth, innerHeight);
-    sk.background(sexyBlack);
-    canves.position(0, 0);
+    const { innerHeight, innerWidth } = window
+    const canves = sk.createCanvas(innerWidth, innerHeight)
+    sk.background(sexyBlack)
+    canves.position(0, 0)
     // canves.style('z-index', '-1');
-    cols = sk.floor(innerWidth / scl);
-    rows = sk.floor(innerHeight / scl);
-    flowfield = new Array(cols * rows);
-    particles = [];
+    cols = sk.floor(innerWidth / scl)
+    rows = sk.floor(innerHeight / scl)
+    flowfield = new Array(cols * rows)
+    particles = []
     for (let i = 0; i < 300; i++) {
-      particles[i] = new Particle(P5, scl, cols);
+      particles[i] = new Particle(P5, scl, cols)
     }
-  };
+  }
 
   sk.draw = () => {
-    var yoff = 0;
+    var yoff = 0
     for (var y = 0; y < rows; y++) {
-      var xoff = 0;
+      var xoff = 0
       for (var x = 0; x < cols; x++) {
-        var index = x + y * cols;
-        var angle = sk.noise(xoff, yoff, zoff) * P5.TWO_PI * 4;
-        var v = Vector.fromAngle(angle);
-        v.setMag(1);
-        flowfield[index] = v;
-        xoff += inc;
-        sk.stroke(2, 50);
+        var index = x + y * cols
+        var angle = sk.noise(xoff, yoff, zoff) * P5.TWO_PI * 4
+        var v = Vector.fromAngle(angle)
+        v.setMag(1)
+        flowfield[index] = v
+        xoff += inc
+        sk.stroke(2, 50)
         // sk.push();
         // sk.translate(x * scl, y * scl);
         // sk.rotate(v.heading());
@@ -45,21 +45,26 @@ let s = (sk) => {
         // sk.line(0, 0, scl, 0);
         // sk.pop();
       }
-      yoff += inc;
+      yoff += inc
 
-      zoff += 0.0003;
+      zoff += 0.0003
     }
     for (var i = 0; i < particles.length; i++) {
-      particles[i].follow(flowfield);
-      particles[i].update();
-      particles[i].edges();
-      particles[i].show();
+      particles[i].follow(flowfield)
+      particles[i].update()
+      particles[i].edges()
+      particles[i].show()
     }
-  };
+  }
   sk.windowResized = () => {
-    const { windowHeight, windowWidth } = sk;
-    sk.resizeCanvas(windowWidth, windowHeight, false);
-  };
-};
+    try {
+      const { windowHeight, windowWidth } = sk
+      // sk.resizeCanvas(windowWidth, windowHeight, false)
+      sk.setup()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
 
-const P5 = new p5(s);
+const P5 = new p5(s)
