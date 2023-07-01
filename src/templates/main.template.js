0f357dbffc05ee7c45a2062cebb92437a50657d3
Rowadz <rowadwo@gmail.com>
2023-07-01 23:55:41 +0200

    remove my photo

--- src/templates/main.template.js
+++ src/templates/main.template.js
@@ -1,23 +1,8 @@
 import { html, render } from 'lit-html'
 
-import imgSrc2 from '../me.webp'
-
 const mainSection = document.getElementById('main')
 mainSection.style.flexDirection = 'column'
 const name = (name) => html`<h1><strong>${name}</strong></h1> `
-render(
-  html`
-    <img
-      loading="lazy"
-      class="lazyload"
-      src="${imgSrc2}"
-      alt="Alt Text!"
-      style="width: 200px; z-index: 200; clip-path: circle(); height: 300px;"
-    />
-
-    ${name('Rowadz')}
-  `,
-  mainSection
-)
+render(html` ${name('Rowadz')} `, mainSection)
 
 export default { displayName: name }
