7fb5965ea69523465537ce947b53151e87819dcc
Rowadz <rowadwo@gmail.com>
2023-07-02 00:05:10 +0200

    fix favicon

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'skeleton-css/css/skeleton.css'
 import './templates/main.template'
 import './templates/p5/intro.p5'
 import './templates/linksSection/links.template'
-import displaySkills from  './templates/skillsSection/skills.template'
+import displaySkills from './templates/skillsSection/skills.template'
 import './templates/githubSection/githubSection.template'
 import './templates/selectedProject/selectedProject.template'
 import './templates/about/about.template'
@@ -12,8 +12,8 @@ import './templates/videos/videos.template'
 
 window.chartsz = []
 window.onresize = () => {
-    window.chartsz.forEach(chart => {
-        chart.resize()
-    });
-    displaySkills()
+  window.chartsz.forEach((chart) => {
+    chart.resize()
+  })
+  displaySkills()
 }
