c92036a7ed3f8341141cc95694737941f0fc8ce2
rowadz <rowadwo@gmail.com>
2026-01-20 23:22:42 +0100

    remove outdated info

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import 'skeleton-css/css/skeleton.css'
 import './templates/main.template'
 import './templates/p5/intro.p5'
 import './templates/linksSection/links.template'
-import displaySkills from './templates/skillsSection/skills.template'
-import './templates/githubSection/githubSection.template'
-import './templates/selectedProject/selectedProject.template'
-import './templates/about/about.template'
-import './templates/videos/videos.template'
+// import displaySkills from './templates/skillsSection/skills.template'
+// import './templates/githubSection/githubSection.template'
+// import './templates/selectedProject/selectedProject.template'
+// import './templates/about/about.template'
+// import './templates/videos/videos.template'
 
 window.chartsz = []
 window.onresize = () => {
